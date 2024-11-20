eb0f59fca3479b1d24d7cdff0651b37125781355
mbimbij <joseph.mbimbi@gmail.com>
2024-11-20 20:04:00 +0100

    Add Mountain to Territory creation

--- src/main/java/org/example/Territory.java
+++ src/main/java/org/example/Territory.java
@@ -2,17 +2,37 @@ package org.example;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Represent 'Madre de Dios' territory. I opted for a noun for the class name and 'Madre de Dios' for the instance name.
+ * Represent 'Madre de Dios' territory. I opted for a noun for the class name and 'Madre de Dios' for the instance
+ * name.
  */
 @Getter
 public class Territory {
     private final int width;
     private final int height;
+    private final List<Mountain> mountains;
 
-    public Territory(int width, int height) {
+    /**
+     * Decided on an all-args constructor instead of a Builder for example. We have all the data at once after reading
+     * the file, and while builders are "elegant", in my experience they tend to hide issues when adding or removing
+     * data, which happens a lot in the early phases of a project and going back and forth on the modelling of the
+     * domain. Granted the call to a validation method in the build() method could possibly address this, a constructor
+     * is simpler and "fails fast(-er)".
+     * <p>
+     * But in the end one or the other is good enough imo, i'm open to discussion, compromise and changing my tune
+     * according to feedback and whether this design gets buy-in from the team or not.
+     *
+     * @param width
+     * @param height
+     * @param mountains
+     */
+    public Territory(int width, int height, List<Mountain> mountains) {
         this.width = width;
         this.height = height;
+        this.mountains = mountains;
         validate();
     }
 
--- src/test/java/org/example/TreasureHuntingApplicationShould.java
+++ src/test/java/org/example/TreasureHuntingApplicationShould.java
@@ -3,7 +3,11 @@ package org.example;
 import net.jqwik.api.*;
 import net.jqwik.api.arbitraries.IntegerArbitrary;
 import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -15,7 +19,7 @@ class TreasureHuntingApplicationShould {
         Integer height = pair.second();
 
         // WHEN
-        Territory madreDeDios = new Territory(width, height);
+        Territory madreDeDios = new Territory(width, height, emptyList());
 
         // THEN
         assertThat(madreDeDios.getWidth()).isEqualTo(width);
@@ -29,12 +33,29 @@ class TreasureHuntingApplicationShould {
         Integer height = pair.second();
 
         // WHEN
-        ThrowableAssert.ThrowingCallable throwingCallable = () -> new Territory(width, height);
+        ThrowableAssert.ThrowingCallable throwingCallable = () -> new Territory(width, height, emptyList());
 
         // THEN
         assertThatThrownBy(throwingCallable)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Width and height must be greater than zero");
+    }
+
+    @Test
+    void create_territory_with_mountains() {
+        // GIVEN
+        List<Mountain> mountains = List.of(
+                new Mountain(1, 1),
+                new Mountain(2, 2)
+        );
+
+        // WHEN
+        Territory madreDeDios = new Territory(3, 4, mountains);
+
+        // THEN
+        assertThat(madreDeDios.getMountains())
+                .usingRecursiveFieldByFieldElementComparator()
+                .isEqualTo(mountains);
     }
 
     @Provide
