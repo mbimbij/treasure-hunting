fd9aea00c36e98bb3acd4a685407dc04f3c40394
mbimbij <joseph.mbimbi@gmail.com>
2024-11-20 15:33:53 +0100

    Initial Commit

---
+++ src/test/java/org/example/TreasureHuntingApplicationShould.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TreasureHuntingApplicationShould {
+    @Test
+    void run_unit_tests() {
+        assertThat(true).isFalse();
+    }
+}
