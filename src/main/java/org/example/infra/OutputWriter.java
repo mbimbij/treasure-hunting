9043d74776e4ba9120e1601ef898b13a78e0a102
mbimbij <joseph.mbimbi@gmail.com>
2024-11-23 14:14:25 +0100

    Fix typo in parameter name

--- src/main/java/org/example/infra/OutputWriter.java
+++ src/main/java/org/example/infra/OutputWriter.java
@@ -10,12 +10,12 @@ import java.nio.file.Paths;
 
 public class OutputWriter {
     @SneakyThrows
-    public static void writeToFile(String formatterSimulation, String pathString) {
+    public static void writeToFile(String formattedSimulation, String pathString) {
         System.out.printf("printing the simulation results to %s%n", pathString);
-        System.out.println(formatterSimulation);
+        System.out.println(formattedSimulation);
         Path path = Paths.get(pathString);
         setupOutputFile(path);
-        Files.writeString(path, formatterSimulation, StandardCharsets.UTF_8);
+        Files.writeString(path, formattedSimulation, StandardCharsets.UTF_8);
     }
     private static void setupOutputFile(Path path) throws IOException {
         Files.deleteIfExists(path);
